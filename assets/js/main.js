02e6ca1d24a1a9786a42d1e874610efcfa0d1b9f
Irina <irensaywhen@gmail.com>
2020-10-19 11:53:48 +0000

    Travis #21

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,64 @@
-// Block scroll when menu is open
+// Mobile menu
+(function () {
+  //Select DOM Items
 
-//Select DOM Items
+  const menuBtn = document.getElementById('menu-btn');
+  const menu = document.getElementById('mobile-menu');
+  const body = document.body;
 
-const menuBtn = document.getElementById('menu-btn');
-const menu = document.getElementById('mobile-menu');
-const body = document.body;
-const clampElements = Array.from(document.querySelectorAll('.clamp-excerpt p'));
-console.log(clampElements);
+  //Set Initial State Of Menu
+  let showMenu = false;
 
-//Set Initial State Of Menu
-let showMenu = false;
+  menuBtn.addEventListener('click', () => {
+    if (!showMenu) {
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      body.classList.add('mobile-menu');
 
-menuBtn.addEventListener('click', () => {
-  if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    body.classList.add('mobile-menu');
+      // Set menu state
+      showMenu = true;
+    } else {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      body.classList.remove('mobile-menu');
 
-    // Set menu state
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    body.classList.remove('mobile-menu');
+      // Set menu state
+      showMenu = false;
+    }
+  });
+})();
 
-    // Set menu state
-    showMenu = false;
+// Text truncation
+(function () {
+  const clampElements = Array.from(
+    document.querySelectorAll('.clamp-excerpt p')
+  );
+  // Clamp text
+  clampElements.forEach(element => $clamp(element, { clamp: 4 }));
+})();
+
+// Copy email address on click
+(function () {
+  const emailAddress = document.getElementById('email').dataset.email;
+
+  function copyToClipboard(text) {
+    // Input to copy text to clipboard
+    var dummy = document.createElement('input');
+
+    document.body.appendChild(dummy);
+
+    dummy.setAttribute('value', text);
+    dummy.select();
+
+    document.execCommand('copy');
+
+    // Delete input
+    document.body.removeChild(dummy);
   }
-});
 
-clampElements.forEach(element => $clamp(element, { clamp: 4 }));
+  email.addEventListener('click', event => {
+    event.preventDefault();
+
+    copyToClipboard(emailAddress);
+  });
+})();
